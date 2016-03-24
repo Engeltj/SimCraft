9e220a0205b6cd763706df382f64ffe8a592f26c
Timothy Engel <engeltj@gmail.com>
2016-03-23 21:06:23 -0400

    Initial commit

---
+++ src/com/antgaming/simcraft/SimCraft.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.antgaming.simcraft;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author tim
+ */
+public final class SimCraft extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return true;
+    }
+
+}
